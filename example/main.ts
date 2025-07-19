84c1ddfaead23fe5b7dcfd3782e29be6f922c96f
Cameron Silvestrini <csilvestrini@google.com>
2025-07-19 19:06:53 +1000

    Fix example server

--- example/main.ts
+++ example/main.ts
@@ -13,8 +13,8 @@ const server = new Server()
   // Serve the generated CSS file as /styles.css.
   .serveFile("/styles.css", "static/styles.gen.css")
   // Serve the bundled JS file (and sourcemap).
-  .serveFile("/app.js", "/dist/app.js")
-  .serveFile("/app.js.map", "/dist/app.js.map");
+  .serveFile("/app.js", "dist/app.js")
+  .serveFile("/app.js.map", "dist/app.js.map");
 
 // Start the Deno HTTP server, and forward all requests to your Rex server.
 Deno.serve((request) => server.handle(request));

9cda61af272487d253414cec6de7fccd16936441
Cameron Silvestrini <csilvestrini@google.com>
2025-07-20 12:19:20 +1000

    Add example usage of flags

--- example/pages/HomePage.tsx
+++ example/pages/HomePage.tsx
@@ -1,5 +1,8 @@
-import type { RexRequest } from "rex";
+import { type RexRequest, StringFlag } from "rex";
 import { ExampleBasePage } from "./ExampleBasePage.tsx";
+
+// Define a flag, whose value is specified in the .env file.
+const myNameFlag = new StringFlag("MY_NAME", "Default Name");
 
 /** Handler for the home page, served at `/`. */
 export class HomePage extends ExampleBasePage {
@@ -12,6 +15,8 @@ export class HomePage extends ExampleBasePage {
         <h1 class="text-center text-5xl">Rex Example Server</h1>
 
         <img src="/icons/dog.png" />
+
+        <p>Hi, {myNameFlag.get()}</p>
       </>
     );
   }
